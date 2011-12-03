090a3b4e6eaff2b2334c56a7b5431fb18cbca532
John D.aker <bakerjd99@gmail.com>
2011-12-03 10:52:25 -0600

    0.9.5 wip revision of jod.pdf
    1) new logo
    2) all \ converted to /
    3) typos fixed
    4) new blank NIMP sections added

--- 0ajod.bat
+++ 0ajod.bat
@@ -1,4 +1,7 @@
+echo off
 rem build jod.pdf (jod.tex) is root file 
+title Running JOD/LaTeX  ...
+
 latex jod
 bibtex jod
 makeindex jod
@@ -8,4 +11,5 @@ dvips jod
 ps2pdf jod.ps
 
 rem display pdf 
+title JOD/LaTeX complete displaying PDF ...
 "C:\Program Files\SumatraPDF\SumatraPDF.exe" jod.pdf
