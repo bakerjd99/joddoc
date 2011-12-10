d38ad0a91998bc9825233a90cadb2c67bcfcce69
John D.aker <bakerjd99@gmail.com>
2011-12-10 12:14:43 -0600

    0.9.6 - New Lulu test edition generated.
    1) Modified front and back covers using jod cube logo.
    2) Updated document text - includes NIMP sections for
       for planned additions.
    3) Source used to generate jodlulu2.pdf - uploaded to Lulu
       on Dec 9, 2011

---
+++ 0fsvn.bat
@@ -0,0 +1,24 @@
+copy jod.tex            C:\jodsvn\joddictionary\jodtex
+copy jodbib.bib         C:\jodsvn\joddictionary\jodtex            
+copy jodamble.tex       C:\jodsvn\joddictionary\jodtex            
+copy jodappendices.tex  C:\jodsvn\joddictionary\jodtex            
+copy jodbest.tex        C:\jodsvn\joddictionary\jodtex            
+copy jodcommon.tex      C:\jodsvn\joddictionary\jodtex\              
+copy jodexample.tex     C:\jodsvn\joddictionary\jodtex\            
+copy jodfiles.tex       C:\jodsvn\joddictionary\jodtex\            
+copy jodhung.tex        C:\jodsvn\joddictionary\jodtex\            
+copy jodintro.tex       C:\jodsvn\joddictionary\jodtex\            
+copy jodscripts.tex     C:\jodsvn\joddictionary\jodtex\           
+copy jodtitlepage.tex   C:\jodsvn\joddictionary\jodtex\           
+copy jodtutor.tex       C:\jodsvn\joddictionary\jodtex\           
+copy jodvcsys.tex       C:\jodsvn\joddictionary\jodtex\          
+copy jodwords.tex       C:\jodsvn\joddictionary\jodtex\          
+copy jodgraphics\j6folders.eps      C:\jodsvn\joddictionary\jodtex\jodgraphics\   
+copy jodgraphics\j6labs.eps         C:\jodsvn\joddictionary\jodtex\jodgraphics\
+copy jodgraphics\j7folders.eps      C:\jodsvn\joddictionary\jodtex\jodgraphics\
+copy jodgraphics\j7labs.eps         C:\jodsvn\joddictionary\jodtex\jodgraphics\
+copy jodgraphics\jodcubelogo01.eps  C:\jodsvn\joddictionary\jodtex\jodgraphics\
+copy jodgraphics\joddot.eps         C:\jodsvn\joddictionary\jodtex\jodgraphics\
+copy jodgraphics\jodlittlecube.eps  C:\jodsvn\joddictionary\jodtex\jodgraphics\
+copy jodgraphics\zippy.eps          C:\jodsvn\joddictionary\jodtex\jodgraphics\ 
+pause
