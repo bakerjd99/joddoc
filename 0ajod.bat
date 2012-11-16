35799fdfc5d91e532019b57b68878564a3529722
John Baker <bakerjd99@hotmail.com>
2012-11-16 14:18:26 -0600

    adjust main *.bat to check presence jaddons jod.pdf location

--- 0ajod.bat
+++ 0ajod.bat
@@ -6,12 +6,13 @@ latex jod
 bibtex jod
 makeindex jod
 latex jod
+makeindex jod
 latex jod
 dvips jod
 ps2pdf jod.ps
 
-rem copy jod.pdf to jodsvn directory & working jod directories
-copy jod.pdf c:\jaddons\general\joddocument\pdfdoc\jod.pdf
+rem copy jod.pdf to jodsvn/github directory & working jod directories
+if exist C:\jaddons\general\joddocument\pdfdoc\jod.pdfcopy copy jod.pdf C:\jaddons\general\joddocument\pdfdoc\jod.pdf
 if exist C:\j701\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j701\addons\general\joddocument\pdfdoc\jod.pdf
 if exist C:\j602\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j602\addons\general\joddocument\pdfdoc\jod.pdf
 
