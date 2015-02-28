311cdd3d83970fcc10adc9929e82b1e31ed1278f
John Baker <bakerjd99@gmail.com>
2015-02-28 12:45:56 -0600

    adjust 0ajod.bat to run on home and work machines

--- 0ajod.bat
+++ 0ajod.bat
@@ -16,15 +16,20 @@ if not exist jod.pdf goto TheEnd
 
 rem copy jod.pdf to working jod directories
 if exist C:\uap\j64-803\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\uap\j64-803\addons\general\joddocument\pdfdoc\jod.pdf 
-if exist C:\j602\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j602\addons\general\joddocument\pdfdoc\jod.pdf
+if exist C:\j64\j64-803\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j64\j64-803\addons\general\joddocument\pdfdoc\jod.pdf
+if exist C:\j32\j602\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j32\j602\addons\general\joddocument\pdfdoc\jod.pdf
 
 rem display pdf 
 title JOD/LaTeX complete displaying PDF ...
 if exist C:\uap\sumatra\SumatraPDF.exe goto Location0 
+if exist C:\PROGRA~2\SumatraPDF\SumatraPDF.exe goto Location1
 goto TheEnd
 
 :Location0
     C:\uap\sumatra\SumatraPDF.exe jod.pdf
     goto TheEnd
-
+:Location1
+	C:\PROGRA~2\SumatraPDF\SumatraPDF.exe jod.pdf
+	goto TheEnd
+	
 :TheEnd
