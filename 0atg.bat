8a3a11e268d7d2c60d80c276fcac2a19e2990da8
John Baker <bakerjd99@gmail.com>
2018-05-26 20:10:39 -0600

    testing new EPS graphics
    I am updating some of the image inclusions in the next  JOD document release to reflect J 8 systems.  jod.pdf is built with dvips and these files test EPS documents created by converting screen shots to EPS with Affinity Photo.

---
+++ 0atg.bat
@@ -0,0 +1,24 @@
+echo off
+rem test new graphics files
+title Running testgraphic  ...
+
+latex testgraphic
+dvips testgraphic
+
+if exist testgraphic.pdf erase testgraphic.pdf
+ps2pdf testgraphic.ps
+if not exist testgraphic.pdf goto TheEnd
+
+title displaying PDF ...
+if exist C:\uap\sumatra\SumatraPDF.exe goto Location0 
+if exist C:\PROGRA~2\SumatraPDF\SumatraPDF.exe goto Location1
+goto TheEnd
+
+:Location0
+    C:\uap\sumatra\SumatraPDF.exe testgraphic.pdf
+    goto TheEnd
+:Location1
+    C:\PROGRA~2\SumatraPDF\SumatraPDF.exe testgraphic.pdf
+    goto TheEnd
+
+:TheEnd
