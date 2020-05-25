975b56ea2cc29984ec7c2ff093e4026961f34551
John Baker <bakerjd99@gmail.com>
2020-05-25 12:16:07 -0600

    jodliterate examples

--- jodliterate.bat
+++ jodliterate.bat
@@ -1,6 +1,9 @@
 rem sequence of latex commands that generate PDF
-rem assumes commands are on the working path
+rem assumes latex exes are on the working path
+setlocal
+cd /d %~dp0
 lualatex  jodliterate
 makeindex jodliterate
 lualatex  jodliterate
 lualatex  jodliterate
+endlocal
--- sunmoon.bat
+++ sunmoon.bat
@@ -2,8 +2,8 @@ rem sequence of latex commands that generate PDF
 rem assumes latex exes are on the working path
 setlocal
 cd /d %~dp0
-lualatex  zzzjodmain
-makeindex zzzjodmain
-lualatex  zzzjodmain
-lualatex  zzzjodmain
+lualatex  sunmoon
+makeindex sunmoon
+lualatex  sunmoon
+lualatex  sunmoon
 endlocal
