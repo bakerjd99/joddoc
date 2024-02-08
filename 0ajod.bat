a1d5928e6e5aa4a1f97025d17a37946620a9cf4a
John Baker <bakerjd99@gmail.com>
2024-02-08 11:44:00 -0700

    update 0ajod.bat paths - minor edits

--- 0ajod.bat
+++ 0ajod.bat
@@ -14,15 +14,18 @@ if exist jod.pdf erase jod.pdf
 ps2pdf jod.ps
 if not exist jod.pdf goto TheEnd
 
+if not exist C:\j64\j950\addons\general\joddocument\pdfdoc\jod.pdf goto Location2
+if not exist C:\j64\j960\addons\general\joddocument\pdfdoc\jod.pdf goto Location2
+if not exist C:\jod\j950\joddev\alien\jodgit\joddocument\pdfdoc\jod.pdf goto Location2
+if not exist C:\jrelease\jaddons\general\joddocument_rel\pdfdoc\jod.pdf goto Location2
+
 rem copy jod.pdf to working jod directories
-if exist C:\j64\j901\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j64\j901\addons\general\joddocument\pdfdoc\jod.pdf
-if exist C:\j64\j902\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j64\j902\addons\general\joddocument\pdfdoc\jod.pdf
-if exist C:\j64\j903\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j64\j903\addons\general\joddocument\pdfdoc\jod.pdf
-if exist C:\j64\j904\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j64\j904\addons\general\joddocument\pdfdoc\jod.pdf
+if exist C:\j64\j950\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j64\j950\addons\general\joddocument\pdfdoc\jod.pdf
+if exist C:\j64\j960\addons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\j64\j960\addons\general\joddocument\pdfdoc\jod.pdf
 
 rem update stage and git release directories
-if exist C:\jod\joddev\alien\stage\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\jod\joddev\alien\stage\joddocument\pdfdoc\jod.pdf
-if exist C:\jrelease\jaddons\general\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\jrelease\jaddons\general\joddocument\pdfdoc\jod.pdf
+if exist C:\jod\j950\joddev\alien\jodgit\joddocument\pdfdoc\jod.pdf copy jod.pdf C:\jod\j950\joddev\alien\jodgit\joddocument\pdfdoc\jod.pdf
+if exist C:\jrelease\jaddons\general\joddocument_rel\pdfdoc\jod.pdf copy jod.pdf C:\jrelease\jaddons\general\joddocument_rel\pdfdoc\jod.pdf
 
 rem display pdf 
 title JOD/LaTeX complete displaying PDF ...
@@ -36,5 +39,9 @@ goto TheEnd
 :Location1
     C:\PROGRA~2\SumatraPDF\SumatraPDF.exe jod.pdf
     goto TheEnd
+:Location2
+    echo jod.pdf distribution paths not set or invalid - edit and rerun
+	pause
+	goto TheEnd
 
 :TheEnd
